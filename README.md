5fceebb50f57b366f93b39d5f56e3439b3f8077f
Joao Pedro Queiroz <joaoprodrigues1108@gmail.com>
2026-05-31 17:49:43 -0300

    Aula01

---
+++ README.md
@@ -0,0 +1,12 @@
+# FastAPI
+#1 poentry python install 3.13
+#2 poetry python list
+#3 poetry env use 3.13
+#4 poetry install
+#5 poetry add fastapi[standard]
+#6 poetry config virtualenvs.in-project true
+#7 poetry shell
+#8 fastapi dev fastapi_zero/app.py
+#9 deactivate
+#10 poetry run fastapi dev fast_zero/app.py
+#11 "C:\Users\Cnt Computadores\AppData\Local\pypoetry\Cache\virtualenvs\fastapi-zero-o0XH6tQl-py3.13\Scripts\Activate.ps1"
---
+++ tests/test_app.py
@@ -0,0 +1,26 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    """
+    Esse teste tem 3 etapas (AAA)
+
+    - A: Arrenge - Arranjo
+    - A: Act     - Executa a coisa (o SUT)
+    - A: Assert  - Garanta que A é A
+
+    """
+
+    # Arrange
+    client = TestClient(app)
+
+    # Act 
+    response = client.get('/')
+
+    # Asset
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Ola mundo'}
