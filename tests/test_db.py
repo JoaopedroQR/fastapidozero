3393d782d51ea2aa7135063f1d0592151c472822
joao pedro <joaoprodrigues1108@gmail.com>
2026-07-07 10:28:35 -0300

    Feat: adicionando a primeira migração com alembic. Tabela de usuários criada.

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,24 +1,34 @@
 from dataclasses import asdict
+
 from sqlalchemy import select
 
 from fast_zero.models import User
 
 
-def test_create_user(session):
-    new_user = User(username='test', email='test@test.com', password='secret')
+def test_create_user(session, mock_db_time):
 
-    session.add(new_user)
-    session.commit()
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='test',
+            email='test@test.com',
+            password='secret',
+        )
 
-    user = session.scalar(
-        select(User).where(User.username == 'test')
-    )
+        session.add(new_user)
+        session.commit()
 
-    # assert user.password == 'secret'
-    assert asdict(user) == {
-        id: 1,
-        'username': 'test',
-        'email': 'test@test.com',
-        'password': 'secret',
-        'created_at': ...
-    }
+        user = session.scalar(select(User).where(User.username == 'test'))
+
+        user.username = "jojo"
+        session.commit()
+        breakpoint()
+
+        # assert user.password == 'secret'
+        assert asdict(user) == {
+            'id': 1,
+            'username': 'test',
+            'email': 'test@test.com',
+            'password': 'secret',
+            'created_at': time,
+            'updated_at': time,
+        }
